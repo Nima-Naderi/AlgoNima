801f47baa50b9a3f7af3cdc149dee6b70732a590
Nima Naderi Ghotbodini <nnad@bendingspoons.com>
2024-04-01 02:53:14 +0200

    feat: add simple templates

---
+++ Template/qry.cpp
@@ -0,0 +1,19 @@
+// #pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll MXN = 2e5 + 10;
+
+void solve(){
+
+}
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);	
+	ll q = 1;
+	cin >> q;
+	while(q --) solve();
+	return 0;
+}
+//! N.N
---
+++ Template/short.cpp
@@ -0,0 +1,14 @@
+// #pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll MXN = 2e5 + 10;
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+	
+
+	return 0;
+}
+//! N.N
