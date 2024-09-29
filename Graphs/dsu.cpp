e6105f16dec99da638aea1ba4b107e29e31ccad5
Nima Naderi <nima.naderi04@gmail.com>
2024-09-29 20:45:35 +0200

    feat: add DSU code

---
+++ Graphs/dsu.cpp
@@ -0,0 +1,33 @@
+// #pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#define Mp make_pair
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+const ll MXN = 3e5 + 10;
+
+ll Par[MXN], Sz[MXN];
+ll Find(ll x){
+    return (Par[x] == x ? x : Par[x] = Find(Par[x]));
+}
+bool Union(ll x, ll y){
+    x = Find(x), y = Find(y);
+    if(x == y) return 0;
+    if(Sz[x] < Sz[y]) swap(x, y);
+    Par[y] = x, Sz[x] += Sz[y];
+	return 1;
+}
+
+int32_t main(){
+	ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);	
+	
+	//initialization
+	iota(Par, Par + n + 1, 0);
+    fill(Sz, Sz + n + 1, 1);
+
+
+	return 0;
+}
+//! N.N
+
