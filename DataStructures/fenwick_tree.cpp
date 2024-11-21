b7e687bd043f7c433ee74cc548015bb571c06aee
Nima Naderi <nima.naderi04@gmail.com>
2024-11-21 21:37:01 +0100

    feat: add logn complexity implementation for binary search on fenwick tree

--- DataStructures/fenwick_tree.cpp
+++ DataStructures/fenwick_tree.cpp
@@ -6,6 +6,7 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> pll;
 const ll MXN = 3e5 + 10;
+const ll LOG = 20;
 ll n;
 
 //Be careful with Mod
@@ -21,6 +22,15 @@ ll Get(ll p){
 }
 ll Get(ll l, ll r){
     return (r < l ? 0 : Get(r) - Get(l - 1));
+}
+ll Find(ll k){
+    ll ans = 0;
+    for(int i = LOG; ~i; i --){
+        if(ans + (1LL << i) < MXN && Fen[ans + (1LL << i)] >= k){
+            ans += (1LL << i), k -= Fen[ans];
+        }
+    }
+    return ans + 1;
 }
 
 int32_t main(){
