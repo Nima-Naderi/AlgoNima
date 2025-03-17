9348eda3543b17efd5b8c9c0eaa65c0ea46fc29d
Nima Naderi <110059240+Nima-Naderi@users.noreply.github.com>
2025-03-17 21:17:33 +0100

    feat: add a good random seed

--- Template/random.cpp
+++ Template/random.cpp
@@ -12,6 +12,7 @@ const ll MXN = 3e5 + 10;
 //mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 //mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 //int uid(int a, int b) { return uniform_int_distribution<int>(a, b)(rng); }
+// mt19937 mrand(19260817);
 mt19937 Rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
 ll uld(ll a, ll b) { return uniform_int_distribution<ll>(a, b)(Rnd); }
 
