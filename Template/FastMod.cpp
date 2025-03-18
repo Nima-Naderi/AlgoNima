5cc3e58c9de9d850e51403f3b6fe54adffb7fbd6
Nima Naderi <nima.naderi04@gmail.com>
2025-03-19 00:33:55 +0100

    feat: add fast modulo inverse calculator for FastMod

--- Template/FastMod.cpp
+++ Template/FastMod.cpp
@@ -22,8 +22,13 @@ int main(){
     ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
     f = FastMod(Mod);
     
-    ll x = Mod + 2;
-    cout << f.reduce(x) << '\n';
+    ll x = Mod + 2; cout << f.reduce(x) << '\n';
+
+    //fast inverse calculation
+    Inv[0] = Inv[1] = 1;
+    for(int i = 2; i < MXN; i ++){
+        Inv[i] = f.reduce(Mod - f.reduce(1ll * Mod / i * Inv[Mod % i]));
+    }
 
     return 0;
 }
