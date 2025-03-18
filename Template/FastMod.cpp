dbba8c298e47c8ddc226dd7118a295bf61d9a5ed
Nima Naderi <nima.naderi04@gmail.com>
2025-03-18 22:22:31 +0100

    feat: add FastMod for fast modular calculation

---
+++ Template/FastMod.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+const ll MXN = 2e5 + 10;
+const ll SQR = 383;
+const ll Mod = 1e9 + 7;
+typedef unsigned long long ull;
+typedef __uint128_t L;
+struct FastMod {
+	ull b, m;
+	FastMod(ull b) : b(b), m(ull((L(1) << 64) / b)) {}
+	ull reduce(ull a) {
+		ull q = (ull)((L(m) * a) >> 64);
+		ull r = a - q * b;
+		return r >= b ? r - b : r;
+	}
+};
+FastMod f(2);
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+    f = FastMod(Mod);
+    
+    ll x = Mod + 2;
+    cout << f.reduce(x) << '\n';
+
+    return 0;
+}
