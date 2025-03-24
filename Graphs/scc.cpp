2a068958fb8529ec5c4eda561377822a24d51ef3
Nima Naderi <nima.naderi04@gmail.com>
2025-03-24 01:58:43 +0100

    perf: optimzing memory usage

--- Graphs/scc.cpp
+++ Graphs/scc.cpp
@@ -7,7 +7,7 @@ const ll MXN = 1e6 + 10;
 ll n, m, comp_cnt;
 ll Comp[MXN];
 vector<ll> adj[MXN], adt[MXN], Top;
-bool mark[MXN], vis[MXN];
+bool mark[MXN];
 void dfs(ll u){
     mark[u] = 1;
     for(auto v : adj[u]){
@@ -16,26 +16,23 @@ void dfs(ll u){
     Top.push_back(u);
 }
 void sfd(ll u){
-    vis[u] = 1, Comp[u] = comp_cnt;
+    mark[u] = 1, Comp[u] = comp_cnt;
     for(auto v : adt[u]){
-        if(!vis[v]) sfd(v);
+        if(!mark[v]) sfd(v);
     }
 }
 int main(){
     ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
     cin >> n >> m;
     for(int i = 1; i <= m; i ++){
-        ll u, v; cin >> u >> v; // directed edge
+        ll u, v; cin >> u >> v; // directed edge"
         adj[u].push_back(v), adt[v].push_back(u);
     }
-    for(int i = 1; i <= n; i ++){
-        if(!mark[i]) dfs(i);
-    }
-    reverse(Top.begin(), Top.end());
-    for(auto u : Top){
-        if(!vis[u]) comp_cnt ++, sfd(u);
-    }
-    
+
+    for(int i = 1; i <= n; i ++) if(!mark[i]) dfs(i);
+    reverse(Top.begin(), Top.end()), fill(mark, mark + n + 1, 0);
+    for(auto u : Top) if(!mark[u]) comp_cnt ++, sfd(u);
+
     return 0;
 }
 //! N.N
