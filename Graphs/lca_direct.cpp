49910a05a24df6542e768d8b0e561b51c0e6e6c4
Nima Naderi <nima.naderi04@gmail.com>
2025-03-28 13:15:24 +0100

    chore: polish code

--- Graphs/lca_direct.cpp
+++ Graphs/lca_direct.cpp
@@ -11,7 +11,6 @@ const ll LOG = 20;
 const ll INF = 1e9;
 ll n;
 vector<ll> adj[MXN];
-
 int lg[MX2N], dis[MXN], last[MXN];
 int RMQ[MX2N][LOG], ind[MX2N][LOG];
 vector<int> Tour;
@@ -30,20 +29,16 @@ inline void InitLog(){
 	for(int i = 0; (1LL << i) < MXN; i ++) lg[(1LL << i)] = i;
 	for(int i = 1; i < MXN; i ++)          lg[i] = max(lg[i - 1], lg[i]);
 }
-void prep(){
-    ll root = 1; dis[root] = 0;
+void prep(ll root){
     TourDFS(root), InitLog();
     for(int i = Tour.size() - 1; i >= 0; i --){
         RMQ[i][0] = Tour[i].first,  ind[i][0] = Tour[i].second;
         for(int j = 1; j < LOG; j ++){
             if(Tour.size() >= (1LL << j)){
                 RMQ[i][j] = min(RMQ[i][j - 1], RMQ[i + (1LL << (j - 1))][j - 1]);
-                if(RMQ[i][j - 1] < RMQ[i + (1LL << (j - 1))][j - 1])    ind[i][j] = ind[i][j - 1];
-                else                                                    ind[i][j] = ind[i + (1LL << (j - 1))][j - 1];
-            }
-            else{
-                RMQ[i][j] = INF;
-            }
+                if(RMQ[i][j - 1] < RMQ[i + (1LL << (j - 1))][j - 1]) ind[i][j] = ind[i][j - 1];
+                else                                                 ind[i][j] = ind[i + (1LL << (j - 1))][j - 1];
+            } else RMQ[i][j] = INF;
         }
     }
     return;
@@ -65,7 +60,7 @@ void solve(){
 		ll u, v; cin >> u >> v;
 		adj[u].push_back(v), adj[v].push_back(u);
 	}
-	dis[1] = 0, prep(1, 0);
+	dis[1] = 0, prep(1);
 
 }
 
