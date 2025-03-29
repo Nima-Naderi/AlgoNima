43ba14fb60e11d191b320ca36d70243549628b8d
Nima Naderi <nima.naderi04@gmail.com>
2025-03-29 10:12:41 +0100

    feat: add the full test of ordered set

--- Template/ordered_set.cpp
+++ Template/ordered_set.cpp
@@ -10,52 +10,23 @@ const ll MXN = 3e5 + 10;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
+ll n, q;
+ordered_set os;
 
 int32_t main(){
 	ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
-	cin >> n;
+	cin >> n >> q;
     for(int i = 1; i <= n; i ++){
         ll x; cin >> x;
-        
+        os.insert(x);
+    }
+    for(int i = 1; i <= q; i ++){
+        ll k, x; cin >> k >> x;
+        cout << *os.find_by_order(k) << " - " << os.order_of_key(x) << '\n';
     }
 
 	return 0;
 }
 //! N.N
-
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-
-// int main() {
-//     int n;
-//     cin >> n;    
-
-//     vector<int> a(n);
-//     vector<pair<int,int>> b;
-//     forn(i, n) {
-//         cin >> a[i];
-//         b.push_back({-a[i], i});
-//     }
-//     sort(b.begin(), b.end());
-
-//     int m;
-//     cin >> m;
-//     vector<pair<pair<int,int>,int>> req(m);
-//     forn(i, m) {
-//         cin >> req[i].first.first >> req[i].first.second;
-//         req[i].second = i;
-//     }
-//     sort(req.begin(), req.end());
-
-//     vector<int> ans(m);
-//     ordered_set pos;
-//     int len = 0;
-//     forn(i, m) {
-//         while (len < req[i].first.first)
-//             pos.insert(b[len++].second);
-//         ans[req[i].second] = a[*pos.find_by_order(req[i].first.second - 1)];
-//     }
-
-//     forn(i, m)
-//         cout << ans[i] << endl;
-// }
