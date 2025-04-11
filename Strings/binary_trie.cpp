dae7bf0b202c378666826f0625fac02bae2546b9
Nima Naderi <nima.naderi04@gmail.com>
2025-04-11 12:07:26 +0200

    feat: add base template for binary trie

---
+++ Strings/binary_trie.cpp
@@ -0,0 +1,23 @@
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define Mp make_pair
+#define MSB(x)  (63 - __builtin_clzll(ll(x)))
+#define bit(x, b) ((x >> b) & 1)
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+const ll MXN = 2e5 + 10;
+const ll Mod = 998244353;
+
+void solve(){
+	
+}
+int32_t main(){
+	ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+	ll tq = 1;
+	cin >> tq;
+	while(tq --) solve();
+	return 0;
+}
+//! N.N
