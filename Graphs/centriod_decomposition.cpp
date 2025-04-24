443ba76f0d81e5227ad4eba2cb4f5a89c0f36642
Nima Naderi <nima.naderi04@gmail.com>
2025-04-24 23:34:12 +0200

    feat: add base for centriod decomposition

---
+++ Graphs/centriod_decomposition.cpp
@@ -0,0 +1,19 @@
+#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+const ll MXN = 1e5 + 10;
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+    
+    return 0;
+}
+/*!
+    HE'S AN INSTIGATOR,
+    ENEMY ELIMINATOR,
+    AND WHEN HE KNOCKS YOU BETTER
+    YOU BETTER LET HIM IN.
+*/
+//! N.N
