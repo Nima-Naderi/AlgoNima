b8822c7f7a6f8742c06db487d97337316d435c04
Nima Naderi <nima.naderi04@gmail.com>
2025-05-05 18:09:13 +0200

    fix: finish the normal BFS implementation

--- Graphs/bfs.cpp
+++ Graphs/bfs.cpp
@@ -8,40 +8,34 @@ typedef pair<ll, ll> pll;
 const ll MXN = 2e5 + 10;
 const ll INF = 1e9 + 10;
 ll n, m;
-ll dis[MXN];
+ll Qq[MXN], dis[MXN];
 vector<ll> adj[MXN];
-bool vis[MXN];
+vector<ll> BFS_Sources;
 inline ll BFS(ll src = 0, ll sink = 0){
+    ll L = 0, R = 0;
+    for(int i = 1; i <= n; i ++) dis[i] = INF;
 	if(src){
-		for(int i = 1; i <= n; i ++) dis[i] = INF, vis[i] = 0;
-		pq.push(Mp(dis[src] = 0, src));
-	} else {
-		//multi-source bfs
-		for(int u = 1; u <= n; u ++) pq.push(Mp(dis[u], u)), vis[u] = 0;
+		dis[src] = 0, Qq[R ++] = src;
+	} else { //multi-source bfs
+		for(auto u : BFS_Sources) dis[u] = 0, Qq[R ++] = u;
 	}
-	while(!pq.empty()){
-		ll u, d; tie(d, u) = pq.top(); pq.pop();
-		if(vis[u]) continue;
-		if(u == sink) return d;
-		vis[u] = 1;
-		for(auto e : adj[u]){
-			ll v, w; tie(v, w) = e;
-			if(!vis[v] && d + w < dis[v]){
-				pq.push(Mp(dis[v] = d + w, v));
-			}
+	while(L != R){
+		ll u = Q[L ++];
+		if(u == sink) return dis[u];
+		for(auto v : adj[u]){
+			if(dis[v] == INF) dis[v] = dis[u] + 1, Qq[R ++] = v;
 		}
 	}
-    //TODO: implent the BFS with array, and L, R pointers
 	return -1;
 }
 int32_t main(){
 	ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);	
 	cin >> n >> m;
 	for(int i = 1; i <= m; i ++){
-		ll u, v, w; cin >> u >> v;
+		ll u, v; cin >> u >> v;
 		adj[u].push_back(v), adj[v].push_back(u);
 	}
-	for(int i = 1; i <= n; i ++) cin >> dis[i];
+	ll k, x; cin >> k; while(k --) { cin >> x, BFS_Sources.push_back(x); }
 	assert(BFS() == -1);
 	for(int i = 1; i <= n; i ++) cout << dis[i] << ' ';
 	return 0;
